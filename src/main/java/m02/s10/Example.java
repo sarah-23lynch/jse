93cc9cfa85351c04a911eac6cb9870246f9b2941
sarah-23lynch <sarah.ricci.gw@gmail.com>
2021-04-28 10:58:41 +0200

    add example

---
+++ src/main/java/m02/s10/Example.java
@@ -0,0 +1,11 @@
+package m02.s10;
+
+public class Example {
+	public static void main(String[] args) {
+      for (int i = 0 ;i < args.length ;i++) {
+        System.out.println("Always"); 
+     
+      }
+	
+	}
+}
